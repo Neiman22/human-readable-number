6a2392386640862ed11dc7778aed3133ba993d76
Artem <106235182+Neiman22@users.noreply.github.com>
2022-07-28 13:08:15 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+const numTw = ['zero', 'one', 'two', 'three', 'four', 'five', 
+'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 
+'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 
+'eighteen', 'nineteen'];
+
+const numHun = ['', '', 'twenty', 'thirty', 'forty',
+    'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+    if (num < 20) return numTw[num];
+    let decimal = num % 10;
+    if (num < 100) return numHun[Math.floor(num/10)] + (decimal? ' ' + numTw[decimal]: '');
+    if (num < 1000) return numTw[Math.floor(num/100)] + ' hundred' + (num % 100 == 0? '':' ' + toReadable(num % 100));
+    if (num < 1000000) return toReadable(Math.floor(num/1000)) + ' thousand' + (num % 1000 == 0? '':' ' + toReadable(num % 1000));
+    return toReadable(Math.floor(num/1000000)) + ' milion' + (num % 1000000 == 0? '':' ' + toReadable(num % 1000000));
 }
